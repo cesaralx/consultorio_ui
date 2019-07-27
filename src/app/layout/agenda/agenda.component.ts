ae50852d55e2bbd2dba9649adb0c867327b91c0e
oscarcferrr <oscarcfer@gmail.com>
2019-07-27 02:14:09 -0500

    Se eagrega función de borrar y actualizar en los registro de forma visual la agenda.component.html

--- src/app/layout/agenda/agenda.component.ts
+++ src/app/layout/agenda/agenda.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 // Full calendar
-import { EventInput, Calendar } from '@fullcalendar/core';
+import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction'; // for selectable
 import listPlugin from '@fullcalendar/list';
 import bootstrapPlugin from '@fullcalendar/bootstrap';
-import { FullCalendarComponent } from '@fullcalendar/angular';
+import esLocale from '@fullcalendar/core/locales/es';
 // other libs
 import Swal from 'sweetalert2';
 import { Observable } from 'rxjs';
@@ -20,20 +20,20 @@ import { CitaModel, PacientModel } from './cita.model';
 import { AgendaService } from './agenda.service';
 import { ConsultoriosService } from '../consultorios/consultorios.service';
 import { TranslateService } from '@ngx-translate/core';
-import { getLocaleTimeFormat } from '@angular/common';
+
 
 
 
 
 @Component({
   selector: 'app-agenda',
-  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './agenda.component.html',
   styleUrls: ['./agenda.component.scss']
 })
 export class AgendaComponent implements OnInit {
   // Variables utilizadas
-  eliminar: boolean = false;
+  title = '';
+  eliminar = false;
   pacientes: PacientModel [] = [];
   cita = new CitaModel();
   citas: CitaModel[] = [];
@@ -43,7 +43,14 @@ export class AgendaComponent implements OnInit {
   closeResult: string;
   // Carga de datos del plugin fullcalendar
   calendarPlugins = [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin, bootstrapPlugin]; // important!
-  calendarEvents: EventInput[] = [];
+  calendarEvents: EventInput;
+  calEvents: EventInput[] = [];
+  locale = esLocale;
+  estatus = [ {color: '#52FF33', status: 'confirmada'},
+              {color: '#3346FF', status: 'nueva'},
+              {color: '#B533FF', status: 'reagendada'},
+              {color: '#FF5733', status: 'cancelada'},
+            ];
 
   constructor(private translate: TranslateService,
               private modal: NgbModal,
@@ -54,29 +61,18 @@ export class AgendaComponent implements OnInit {
     this.getPacientes();
     this.getConsultorios();
     this.getCitas();
-    // console.log(this.calendarEvents);
-  }
-  modifyTitle(eventIndex, newTitle) {
-    const calendarEvents = this.calendarEvents.slice(); // a clone
-    const singleEvent = Object.assign({}, calendarEvents[eventIndex]); // a clone
-    singleEvent.title = newTitle;
-    calendarEvents[eventIndex] = singleEvent;
-    this.calendarEvents = calendarEvents; // reassign the array
+   // this.calendarEvents = this.calEvents;
   }
 
   handleDateClick(arg, content) {
     this.open(content);
     this.eliminar = true;
+    this.title = 'Nueva';
     this.cita = new CitaModel();
     this.cita.fecha = arg.dateStr;
-    console.log(arg);
-    //if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
-    //  this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
-    //    title: 'New Event',
-    //    start: arg.date,
-    //    allDay: arg.allDay
-    //  });
-    //}
+    this.args = arg;
+    // console.log('cita:', this.cita);
+    // console.log('args', arg);
    }
 
 open(content) {
@@ -88,26 +84,23 @@ open(content) {
 }
 eventClick(arg, content) {
   this.args = arg;
-   console.log('argumentitos',this.args);
-
+   console.log('argumentitos', this.args);
+   this.title = 'Modificación de cita';
    this.eliminar = false;
-   //console.log(arg.event);
+   // console.log(arg.event);
    this.agendaService.getCita(arg.event.id).subscribe( (resp: any) => {
    // console.log('Respuesta de agenda', resp);
-    this.cita = resp; 
+    this.cita = resp ;
   });
   this.open(content);
 
  }
-
  getPacientes() {
   this.agendaService.getPacientes()
         .subscribe( (resp: any) => {
             this.pacientes = resp;
-            console.log('pacientes: ', this.pacientes);
         });
 }
-
 getConsultorios() {
   this.consultoriosService.getConsultorios()
             .subscribe( (resp: any) => {
@@ -116,10 +109,22 @@ getConsultorios() {
  }
 
  getCitas() {
+  this.calendarEvents = [];
+  this.calEvents = [];
   this.agendaService.getCitas()
       .subscribe( (resp: any) => {
        this.citas = resp;
-       this.llenarArreglo();
+      if (this.citas === null) { return [] }
+      this.citas.forEach( cita => {
+        const resultado = this.estatus.find(busca => busca.status === cita.status);
+          this.calEvents.push({
+          id: cita._id,
+          start: cita.fecha,
+          title: cita.extra,
+          backgroundColor: resultado.color
+        });
+      });
+      this.calendarEvents = this.calEvents;
   });
  }
 
@@ -135,14 +140,14 @@ getConsultorios() {
   let peticion: Observable <any>;
   if (!this.cita._id) {
   peticion = this.agendaService.altaCita(this.cita);
-  // this.consultorios.push(this.consultorio);
   } else {
     console.log('actualizar');
    peticion = this.agendaService.actualizaCita(this.cita);
   }
       // console.log(this.consultorio);
       peticion.subscribe( resp => {
-        
+        this.getCitas();
+        console.log('respuesta del request ', resp);
         Swal.fire({
           title: 'Actualizo',
           text: 'Se actualizo correctamente',
@@ -161,21 +166,12 @@ getConsultorios() {
       showCancelButton: true
      }).then( resp => {
          if ( resp.value ) {
-           this.agendaService.borrarCita(this.cita._id).subscribe( (resp: any) => {
-            console.log(resp);
+            this.agendaService.borrarCita(this.cita._id).subscribe( ( resp: any ) => {
+            const eventIndex = this.calendarEvents.findIndex( event => event.id === this.args.id);
+            this.calendarEvents.splice(eventIndex, 1);
+            this.calEvents = [...this.calEvents];
            });
          }
      });
-  }
-
-  llenarArreglo() {
-    if (this.citas === null){return []}
-    this.citas.forEach( cita => {
-      this.calendarEvents.push({
-        id: cita._id,
-        start: cita.fecha,
-        title: cita.extra
-      });
-    });
   }
 }
--- src/app/layout/agenda/cita.model.ts
+++ src/app/layout/agenda/cita.model.ts
@@ -1,11 +1,11 @@
 export class CitaModel {
     _id: string;
-    id_paciente: '';
-    id_consultorio: '';
+    id_paciente: string = '';
+    id_consultorio: string = '';
     id_usuario: string;
     fecha: Date;
     fecha_update: Date;
-    status: {type: String, enum: ['confirmada', 'nueva', 'reagendada', 'cancelada'], default: 'nueva'};
+    status: string = 'nueva';
     costo: string;
     extra: string;
 }
