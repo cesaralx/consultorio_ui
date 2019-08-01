f199296aca5af0bb5d0d8bb5bb86c51223b877dd
oscarcferrr <oscarcfer@gmail.com>
2019-07-31 19:16:37 -0500

    se generan archivos del componente visita-medica

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -7,6 +7,7 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { VisitaMedicaComponent } from './visita-medica/visita-medica.component';
 
 
 
@@ -17,6 +18,6 @@ import { HeaderComponent } from './components/header/header.component';
         TranslateModule,
         NgbDropdownModule
     ],
-    declarations: [LayoutComponent, SidebarComponent, HeaderComponent]
+    declarations: [LayoutComponent, SidebarComponent, HeaderComponent, VisitaMedicaComponent]
 })
 export class LayoutModule {}
